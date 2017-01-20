a8a68ab228f516e2e6b335a96a7d61326ab80c10
Shunichi Takagi <shunichi_takagi@jbat.co.jp>
2017-01-20 14:48:46 +0900

    APIを呼び出し先をAzureに変更

--- src/app_modules/constants.js
+++ src/app_modules/constants.js
@@ -2,6 +2,6 @@ angular.module('constants',[]);
 angular.module('constants').constant('ApiUrlConstant',(function(){
 
     return {
-        base_path: "http://localhost:8475/api/",
+        base_path: "http://circle-deploytest.japaneast.cloudapp.azure.com/api/",
     }
 })());
