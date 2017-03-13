6995b1559125e9b8d5aff349a2f4c8030ad3cad2
Shunichi <shunichi_takagi@jbat.co.jp>
2017-03-13 10:17:56 +0900

    API接続先をcircle-apiに変更

--- src/app_modules/constants.js
+++ src/app_modules/constants.js
@@ -2,6 +2,6 @@ angular.module('constants',[]);
 angular.module('constants').constant('ApiUrlConstant',(function(){
 
     return {
-        base_path: "http://circle-deploytest.japaneast.cloudapp.azure.com/api/",
+        base_path: "http://circle-api.japaneast.cloudapp.azure.com/api/",
     }
 })());
